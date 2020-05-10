f14a12bfd57bbdfe82ffde7066dc8958a62b27a5
Yonatan Poch <yonatanpoch55@gmail.com>
2020-05-10 18:35:53 +0300

    started to work on the DB

---
+++ trivia_project/trivia_project/SqliteDataBase.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "IDatabase.h"
+#include "sqlite3.h"
+#include <map>
+
+class SqliteDataBase;
+static std::unordered_map<string, string> users_list;
+
+class SqliteDataBase : 
+	public IDatabase
+{
+public:
+	SqliteDataBase() { openDB; }
+
+	//queries
+	virtual bool doesUserExist(string username);
+	virtual bool doesPasswordMatch(string username, string password);
+	virtual void addNewUser(string username, string password, string email);
+
+
+	bool openDB();
+
+	sqlite3* db;
+	const char* dbFileName;
+};
+
+
+int users_callback(void* data, int argc, char** argv, char** azColName);
