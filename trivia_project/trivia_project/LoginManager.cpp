33d6c9a7bc3b52eaf0fcdbaab5bf9f7b77760339
Yonatan Poch <yonatanpoch55@gmail.com>
2020-05-14 18:05:58 +0300

    tani is teaching me things

--- trivia_project/trivia_project/LoginManager.cpp
+++ trivia_project/trivia_project/LoginManager.cpp
@@ -5,3 +5,20 @@ LoginManager::LoginManager(IDatabasePtr database) :
 	m_database(database)
 {
 }
+
+void LoginManager::signup(string username, string password, string email)
+{
+	this->m_database->addNewUser(username, password, email);
+	LoggedUser user(username);
+	this->m_loggedUsers.push_back(user);
+}
+
+void LoginManager::login(string username, string password)
+{
+	this->m_database->doesPasswordMatch(username, password);
+}
+
+void LoginManager::logout(string username)
+{
+}
+
--- trivia_project/trivia_project/LoginManager.h
+++ trivia_project/trivia_project/LoginManager.h
@@ -1,16 +1,24 @@
-
 #pragma once
 #include "pch.h"
+#include "SqliteDataBase.h"
 #include "IDatabase.h"
-
-//class RequestHandlerFactory;
+#include "LoggedUser.h"
+#include <vector>
+#include <string>
+using std::vector;
+using std::string;
 
 class LoginManager
 {
 public:
 	LoginManager(IDatabasePtr database);
 
+	void signup(string username, string password, string email);
+	void login(string username, string password);
+	void logout(string username);
+
 private:
 	IDatabasePtr m_database;
+	vector<LoggedUser> m_loggedUsers;
 };
 
