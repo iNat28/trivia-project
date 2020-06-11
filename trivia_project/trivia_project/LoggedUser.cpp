b4ade01c0495902f5e0ae2aa3bf914b0dc1a6785
Yonatan Poch <yonatanpoch55@gmail.com>
2020-06-11 18:50:33 +0300

    changed comment

--- trivia_project/trivia_project/LoggedUser.cpp
+++ trivia_project/trivia_project/LoggedUser.cpp
@@ -9,7 +9,7 @@ LoggedUser::LoggedUser(string username) :
 LoggedUser::LoggedUser()
 {
 }
-//TODO: add the rest of the vars into the json funcions
+//TODO: add the rest of the vars into the json funcions and into the keys
 void to_json(json& j, const LoggedUser& loggedUser)
 {
 	j[Keys::username] = loggedUser.username;
