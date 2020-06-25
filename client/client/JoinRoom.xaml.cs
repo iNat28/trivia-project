9500029ffd966d9494622cb9d334dcb7476292cc
Yonatan Poch <yonatanpoch55@gmail.com>
2020-06-25 17:51:23 +0300

    working on the join room code

--- client/client/JoinRoom.xaml.cs
+++ client/client/JoinRoom.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,15 +20,39 @@ namespace client
     /// </summary>
     public partial class JoinRoom : Window
     {
+        private int roomId;
         public JoinRoom()
         {
             InitializeComponent();
+
+           
+            Stream.Send(new JObject(), Codes.GET_ROOM);
+
+            Response response = Stream.Recieve();
+
+            if (Stream.Response(response, Codes.GET_ROOM, this.ErrorBox))
+            {
+                this.ErrorBox.Text = response.jObject.ToString();
+            }
         }
 
         private void JoinRoomButton_Click(object sender, RoutedEventArgs e)
         {
-            if(this.RoomsList.SelectedItem != null)
-                Utils.OpenWindow(this, new Room(false, this.RoomsList.SelectedItem.ToString(), 0, 0));
+            if (this.RoomsList.SelectedItem != null)
+            {
+                JObject jObject = new JObject
+                {
+                    ["roomId"] = this.roomId               
+                };
+                Stream.Send(jObject, Codes.JOIN_ROOM);
+
+                Response response = Stream.Recieve();
+
+                if (Stream.Response(response, Codes.JOIN_ROOM, this.ErrorBox))
+                {
+                    Utils.OpenWindow(this, new Room(false, this.RoomsList.SelectedItem.ToString(), 0, 0));
+                }          
+            }
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
